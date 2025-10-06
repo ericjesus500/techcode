943684b11d73d4cb1d2756dd48a43f75ac5c1f6d
ericjesus500 <jesus50.devweb@gmail.com>
2025-10-06 17:56:33 -0500

    add diseño de cards en el modulo de proyectos, se elimina carpetas de imgs

--- app/Views/project/content.php
+++ app/Views/project/content.php
@@ -5,61 +5,100 @@
   </p>
   <div class="mt-8 flex justify-center">
     <div class="grid grid-cols-3 gap-4 max-w-full">
-      <!-- CARD 1 -->
-      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <!-- CARD 1: HTML+CSS -->
+      <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
         <!-- Encabezado de la tarjeta -->
-        <div class="px-6 py-4 bg-blue-50 dark:bg-blue-900/30 border-b border-gray-200">
-          <h3 class="font-bold text-xl text-blue-800 dark:text-blue-300">📚 HTML + CSS</h3>
+        <div class="px-6 py-4 border-b border-gray-200 h-48 border-1 dark:border-blue-400 overflow-hidden">
+          <img class="w-full h-full object-contain" src="/img/logos/html_css.png" alt="html + css logos">
         </div>
         <!-- Cuerpo de la tarjeta -->
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">Título de la tarjeta</div>
-          <p class="text-gray-700 text-base">
-            Algún texto de ejemplo para construir sobre el título de la tarjeta y hacer la mayor parte del contenido de la tarjeta.
-          </p>
+        <div class="px-6 py-4 h-48">
+          <h3 class="font-bold mt-2 text-xl bg-blue-50 dark:bg-blue-900/30 text-blue-800 dark:text-blue-300">📚 HTML + CSS</h3>
+          <p class="font-mono mt-5">Proyectos básicos y avanzados con nuevas propiedades de CSS actualizadas al 2025</p>
         </div>
         <!-- Pie de página de la tarjeta -->
         <div class="px-6 py-4 border-t border-gray-200 text-sm text-gray-500">
-          Ver proyectos
+          <a class="underline cursor-pointer">Ver proyectos</a>
         </div>
       </div>
-      <!-- CARD 2 -->
+      <!-- CARD 2: JAVASCRIPT -->
       <div class="max-w-sm rounded overflow-hidden shadow-lg">
         <!-- Encabezado de la tarjeta -->
-        <div class="px-6 py-4 bg-green-50 dark:bg-green-900/30 border-b border-gray-200">
-          <div class="font-bold text-xl">JAVASCRIPT</div>
+        <div class="px-6 py-4 border-b border-gray-200 h-48 border-1 dark:border-blue-400 overflow-hidden">
+          <img class="w-full h-full object-contain" src="/img/logos/js.png" alt="js logo">
         </div>
         <!-- Cuerpo de la tarjeta -->
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">Título de la tarjeta</div>
-          <p class="text-gray-700 text-base">
-            Algún texto de ejemplo para construir sobre el título de la tarjeta y hacer la mayor parte del contenido de la tarjeta.
-          </p>
+        <div class="px-6 py-4 h-48">
+          <h3 class="font-bold mt-2 text-xl bg-green-50 dark:bg-green-900/30 text-blue-800 dark:text-blue-300"">📒 JAVASCRIPT</h3>
+          <p class=" font-mono mt-5">Proyectos básicos y avanzados con nuevas propiedades de JS 2024</p>
         </div>
         <!-- Pie de página de la tarjeta -->
         <div class="px-6 py-4 border-t border-gray-200 text-sm text-gray-500">
-          Ver proyectos
+          <a class="underline cursor-pointer">Ver proyectos</a>
         </div>
       </div>
-      <!-- CARD 3 -->
+      <!-- CARD 3: PHP -->
       <div class="max-w-sm rounded overflow-hidden shadow-lg">
         <!-- Encabezado de la tarjeta -->
-        <div class="px-6 py-4 bg-purple-50 dark:bg-purple-900/30 border-b border-gray-200">
-          <div class="font-bold text-xl">PHP</div>
+        <div class="px-6 py-4 border-b border-gray-200 h-48 border-1 dark:border-blue-400 overflow-hidden">
+          <img class="w-full h-full object-contain" src="/img/logos/php.png" alt="php logo">
         </div>
         <!-- Cuerpo de la tarjeta -->
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">Título de la tarjeta</div>
-          <p class="text-gray-700 text-base">
-            Algún texto de ejemplo para construir sobre el título de la tarjeta y hacer la mayor parte del contenido de la tarjeta.
-          </p>
+        <div class="px-6 py-4 h-48">
+          <h3 class="font-bold mt-2 text-xl bg-purple-50 dark:bg-purple-900/30 text-blue-800 dark:text-blue-300"">📘 PHP</h3>
+        </div>
+        <!-- Pie de página de la tarjeta -->
+        <div class=" px-6 py-4 border-t border-gray-200 text-sm text-gray-500">
+            <a class="underline cursor-pointer">Ver proyectos</a>
+        </div>
+      </div>
+      <!-- CARD 4: GIT -->
+      <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+        <!-- Encabezado de la tarjeta -->
+        <div class="px-6 py-4 border-b border-gray-200 h-48 border-1 dark:border-blue-400 overflow-hidden">
+          <img class="w-full h-full object-contain" src="/img/logos/git.png" alt="html + css logos">
+        </div>
+        <!-- Cuerpo de la tarjeta -->
+        <div class="px-6 py-4 h-48">
+          <h3 class="font-bold mt-2 text-xl bg-blue-50 dark:bg-blue-900/30 text-blue-800 dark:text-blue-300">📚 GIT</h3>
+          <p class="font-mono mt-5">Proyectos básicos y avanzados con nuevas propiedades de CSS actualizadas al 2025</p>
         </div>
         <!-- Pie de página de la tarjeta -->
         <div class="px-6 py-4 border-t border-gray-200 text-sm text-gray-500">
-          Ver proyectos
+          <a class="underline cursor-pointer">Ver proyectos</a>
         </div>
       </div>
-
+      <!-- CARD 5: LARAVEL -->
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <!-- Encabezado de la tarjeta -->
+        <div class="px-6 py-4 border-b border-gray-200 h-48 border-1 dark:border-blue-400 overflow-hidden">
+          <img class="w-full h-full object-contain" src="/img/logos/laravel.png" alt="js logo">
+        </div>
+        <!-- Cuerpo de la tarjeta -->
+        <div class="px-6 py-4 h-48">
+          <h3 class="font-bold mt-2 text-xl bg-green-50 dark:bg-green-900/30 text-blue-800 dark:text-blue-300"">📒 LARAVEL</h3>
+          <p class=" font-mono mt-5">Proyectos básicos y avanzados con nuevas propiedades de JS 2024</p>
+        </div>
+        <!-- Pie de página de la tarjeta -->
+        <div class="px-6 py-4 border-t border-gray-200 text-sm text-gray-500">
+          <a class="underline cursor-pointer">Ver proyectos</a>
+        </div>
+      </div>
+      <!-- CARD 6: TAILWIND -->
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <!-- Encabezado de la tarjeta -->
+        <div class="px-6 py-4 border-b border-gray-200 h-48 border-1 dark:border-blue-400 overflow-hidden">
+          <img class="w-full h-full object-contain" src="/img/logos/tailwind.png" alt="php logo">
+        </div>
+        <!-- Cuerpo de la tarjeta -->
+        <div class="px-6 py-4 h-48">
+          <h3 class="font-bold mt-2 text-xl bg-purple-50 dark:bg-purple-900/30 text-blue-800 dark:text-blue-300"">📘 TAILWIND</h3>
+        </div>
+        <!-- Pie de página de la tarjeta -->
+        <div class=" px-6 py-4 border-t border-gray-200 text-sm text-gray-500">
+            <a class="underline cursor-pointer">Ver proyectos</a>
+        </div>
+      </div>
     </div>
   </div>
 </div>
